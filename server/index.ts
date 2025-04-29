36483d61abc3c38b80a3f69192055ce9f6c9219f
anshmourya <anshmourya657@gmail.com>
2025-04-29 22:25:03 +0530

    feat: integrate higging face api

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,10 @@ import express from "express";
 import cors from "cors";
 import multer from "multer";
 import { Queue } from "bullmq";
+import { vectorStore } from "./worker/worker";
+import { OpenAI } from "openai";
 const app = express();
+
 app.use(cors());
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -14,6 +17,10 @@ const storage = multer.diskStorage({
   },
 });
 
+const client = new OpenAI({
+  baseURL: "https://router.huggingface.co/nebius/v1",
+  apiKey: process.env.HUGGING_FACE_API,
+});
 const fileQueue = new Queue("fileQueue", {
   connection: {
     host: "localhost",
@@ -42,6 +49,41 @@ app.post("/upload", upload.single("file"), (req, res) => {
   res.status(200).json({
     message: "file recived",
   });
+});
+
+app.get("/chat", async (req: express.Request, res: express.Response) => {
+  try {
+    const data = await vectorStore.similaritySearch("who is ansh?", 2);
+    const SYSTEM_PROMPT = `You are a helpful assistant. You have access to the following documents and you can answer questions based on them:
+    ${data.map((doc) => doc.pageContent).join("\n\n")}`;
+
+    const chatCompletion = await client.chat.completions.create({
+      model: "deepseek-ai/DeepSeek-V3",
+      messages: [
+        {
+          role: "system",
+          content: SYSTEM_PROMPT,
+        },
+        {
+          role: "user",
+          content: "who is ansh?",
+        },
+      ],
+      max_tokens: 512,
+    });
+
+    const response = chatCompletion.choices[0].message.content;
+    console.log(response);
+    return res.json({
+      message: "here",
+      data: response,
+      SYSTEM_PROMPT,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error instanceof Error ? error.message : "Unknown error",
+    });
+  }
 });
 
 app.listen(8000, () => {
--- server/worker/worker.ts
+++ server/worker/worker.ts
@@ -1,11 +1,20 @@
-import { Worker, Job, tryCatch } from "bullmq";
+import { Worker } from "bullmq";
 import { QdrantVectorStore } from "@langchain/qdrant";
-import { OpenAIEmbeddings } from "@langchain/openai";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-import { Document } from "langchain/document";
 import { CharacterTextSplitter } from "langchain/text_splitter";
-import { QdrantClient } from "@qdrant/js-client-rest";
-const filteWorket = new Worker(
+import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
+
+export const vectorStore = new QdrantVectorStore(
+  new HuggingFaceInferenceEmbeddings({
+    apiKey: process.env.HUGGING_FACE_API,
+  }),
+  {
+    url: "http://localhost:6333",
+    collectionName: "pdf-docs",
+  }
+);
+
+const worker = new Worker(
   "fileQueue",
   async (job) => {
     try {
@@ -18,19 +27,7 @@ const filteWorket = new Worker(
         chunkOverlap: 0,
       });
 
-      const text = await splitter.createDocuments([docs[0].pageContent]);
-      const embeddings = new OpenAIEmbeddings({
-        model: "text-embedding-3-small",
-        apiKey: "",
-      });
-
-      const vectorStore = await QdrantVectorStore.fromExistingCollection(
-        embeddings,
-        {
-          url: "http://localhost:6333",
-          collectionName: "pdf-docs",
-        }
-      );
+      const text = await splitter.splitDocuments(docs);
 
       await vectorStore.addDocuments(text);
       console.log("done");
